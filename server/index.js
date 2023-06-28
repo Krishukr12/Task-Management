f9fbe687659c2c4aa941075b0577ede9cc0fc400
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-06-28 12:27:26 +0530

    Iitial commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+// All imports
+const express = require("express");
+const connection = require("./config/db");
+const createError = require("./utils/createError.js");
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("You are good to go");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const errStatus = err.status || 500;
+  const errMessage = err.message || "something went wrong";
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log("mongoDb connection established");
+  } catch (error) {
+    console.log("Error while connecting mongoDB");
+  }
+});
